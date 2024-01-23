12afbc94eb800e59ed199ebbf3026c870ee892c9
Camilo <camilo_hd@yahoo.com>
2024-01-23 12:35:34 +0100

    ajout menu
    ajout du menu de début

---
+++ scripts/menuDebut.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_play_pressed():
+	get_tree().change_scene("res://scenes/niveaux/M0_Tuto/Niveau_0.tscn")
+
+
+func _on_button_settings_pressed():
+	get_tree().change_scene("res://scenes/Option.tscn")
+
+
+func _on_button_quit_pressed():
+	get_tree().quit()
