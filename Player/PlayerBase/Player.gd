03cfd323a3144b7009ef5716ef40a6eea5084ebd
Tlax <maxime.leroy5445@gmail.com>
2024-01-24 07:44:39 +0100

    feat: animation perso principale

--- Player/PlayerBase/Player.gd
+++ Player/PlayerBase/Player.gd
@@ -3,33 +3,72 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var is_transforming_to = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = $AnimationPlayer
 
 func _physics_process(delta):
-	
 	if Donnee.menu_visible :
 		velocity.y=0
 		velocity.x=0
+	
+	if is_transforming_to != null:
+		if anim.is_playing():
+			return
+		else:
+			$"..".switch_player(is_transforming_to)
+			is_transforming_to = null
+	
+	
+	
 	# Add the gravity.
-	if  !is_on_floor() && !Donnee.menu_visible:
+	if not is_on_floor():
 		velocity.y += gravity * delta
-	# Add the gravity.
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") && is_on_floor() && !Donnee.menu_visible:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		anim.play("jump")
+		
+	
+	#Transformation
+	
+	elif Input.is_action_just_pressed("transform_fire"):
+		anim.play("Transfo_feu")
+		is_transforming_to = "fire"
+		
+	elif Input.is_action_just_pressed("transform_water"):
+		anim.play("Transfo_eau")
+		is_transforming_to = "water"
+		
+	elif Input.is_action_just_pressed("transform_air"):
+		$"..".switch_player("air")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction && !Donnee.menu_visible:
-		velocity.x = direction * SPEED
+	
 	else:
-		if !direction && !Donnee.menu_visible :
+		var direction = Input.get_axis("move_left", "move_right")
+		if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+		elif direction == 1:
+			$AnimatedSprite2D.flip_h = false
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.y == 0:
+				anim.play("run")
+		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if velocity.y == 0:
+				anim.play("idle")
+		
+		
+		if velocity.y > 0:
+			anim.play("fall")
+	
+	
 
 	move_and_slide()
--- scenes/niveaux/M_1/niveau_1.gd
+++ scenes/niveaux/M_1/niveau_1.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
+var current_player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Donnee.monde_actuel=1
 	Donnee.niveau_actuel=1
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#NiveauGeneral.get_input()
-	#NiveauGeneral.position_actuel= get_node("CharacterBody2D").position
-	#print(NiveauGeneral.position_actuel)
-	pass
+	current_player = $Spawner/Player_Base
+	$Spawner.spawn()
